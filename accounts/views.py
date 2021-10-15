d0a4a5d8cb7489a386141517501ca1b575d5c131
saguillo2000 <oriolsaguilo@gmail.com>
2021-10-15 02:46:46 +0200

    Log in da el token de auth

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from accounts import serializers, models
+from accounts import serializers, models, permissions
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permissions_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)  # How to do searches of data
+    search_fields = ('name', 'email',)
 
-# Create your views here.
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
--- booksy/settings.py
+++ booksy/settings.py
@@ -28,7 +28,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
