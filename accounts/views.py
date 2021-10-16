24861dc23917375b34543df26276e8be0abbead3
dragris <dragris1234@gmail.com>
2021-10-16 12:04:20 +0200

    Updated UserProfileViewSet so the get function of all the accounts cannot be done.

--- accounts/views.py
+++ accounts/views.py
@@ -10,13 +10,14 @@ from rest_framework.settings import api_settings
 from accounts import serializers, models, permissions
 
 
-class UserProfileViewSet(viewsets.ModelViewSet):
+class UserProfileViewSet(viewsets.ModelViewSet):  # Viewset has DEFAULT_RENDERER_CLASSES by default, no need to add it
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permissions_classes = (permissions.UpdateOwnProfile,)
-    filter_backends = (filters.SearchFilter,)  # How to do searches of data
-    search_fields = ('name', 'email',)
+    # filter_backends = (filters.SearchFilter,)  # How to do searches of data
+    # search_fields = ('name', 'email',)  # Searching by name or email
+    http_method_names = ['post']  # Here you can put the names of the requests that this viewSet is able to process
 
 
 class UserLoginApiView(ObtainAuthToken):
