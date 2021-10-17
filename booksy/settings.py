3752841afa90aed7a96e3e001ac2275e8f60b9cf
Eric <eriduma@gmail.com>
2021-10-17 11:36:47 +0200

    deployment

--- booksy/settings.py
+++ booksy/settings.py
@@ -80,25 +80,16 @@ WSGI_APPLICATION = 'booksy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd65tmc6ari7ok9',
+        'USER': 'fcttpehgspvplr',
+        'PASSWORD': '6aa062d85c852b3c647aa86f534efc3ee3646cb2bfbd5b055f812027427a0126',
+        'HOST': 'ec2-52-6-77-239.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
 # Deployment database
-# TODO change 'HOST' to correct Heroku address
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'booksy',
-        'USER': 'admin',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
