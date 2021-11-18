d7eb8d2ff7e11ab5b00e1a77adb859abd4e6bb60
Eric <eriduma@gmail.com>
2021-11-18 12:24:05 +0100

    fixed homepage breaking when not logged in, it now displays the default values of an item when updating.

--- frontend/src/components/UpdateItem.js
+++ frontend/src/components/UpdateItem.js
@@ -17,7 +17,7 @@ class UpdateItem extends Component {
         super(props);
         this.state = {
             id: this.props.location.state.id,
-            seller: (window.localStorage.getItem('user_id')).toString(),
+            seller: (window.localStorage.getItem('user_id')),
             title: '',
             price: 0,
             author: '',
@@ -26,11 +26,12 @@ class UpdateItem extends Component {
             categories: {},
             card_id: props.id
         }
-        this.getCategories()
-        this.handleChange = this.handleChange.bind(this);
     }
 
-    componentWillMount() {
+    componentDidMount() {
+        this.getInfoToUpdate = this.getInfoToUpdate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.getCategories()
         this.getInfoToUpdate()
     }
 
@@ -47,7 +48,7 @@ class UpdateItem extends Component {
         });
     };
 
-    getInfoToUpdate () {
+    getInfoToUpdate() {
         axios.get(`${url}/api/product/?id=${this.state.id}`)
             .then((res) => {
                 this.state.title = res.data[0].title
@@ -56,9 +57,9 @@ class UpdateItem extends Component {
                 this.state.price = res.data[0].price
                 this.state.shown_category = res.data[0].category['category_description']
                 this.state.category = res.data[0].category['category_name']
+                this.setState(this.state)
 
                 //TODO recuperar las imagenes y bien las categorias
-
                 //this.getCategory(res.data[0].category)
             })
             .catch((error) => {
@@ -76,15 +77,14 @@ class UpdateItem extends Component {
     updateItem = event => {
         //We are using FormData because the backend needs a form-encoded data (request.POST)
         let formItem = new FormData()
+        formItem.append('id', this.state.id)
         formItem.append('title',this.state.title)
         formItem.append('price',this.state.price)
         formItem.append('author',this.state.author)
         formItem.append('category', this.state.category)
-        for (let i; i < this.state.categories.length; i++){
-
-        }
         formItem.append('seller', this.state.seller)
         formItem.append('description',this.state.description)
+
         if (this.checkFormParams(formItem)) {
             axios.patch(`${url}/api/product/?id=${this.state.id}`, formItem,
                 {headers: {'Authorization': `Token ${window.localStorage.getItem('token')}`}})
@@ -146,7 +146,6 @@ class UpdateItem extends Component {
 
     renderCategories = () => {
         const newCategories = Object.getOwnPropertyNames(this.state.categories);
-
         return newCategories.map((cat, i) => (
             <option value={cat}>{this.state.categories[cat]}</option>
         ));
@@ -197,13 +196,12 @@ class UpdateItem extends Component {
                                 </div>
                                 <br/>
                                 <div className="input-field">
-                                    <input type="number" min="0" id="price" onChange={this.handleChange} required defaultValue={this.state.price}/>
+                                    <input type="number" min="0" id="price" onChange={this.handleChange} required value={this.state.price}/>
                                     <label htmlFor="price">Precio del objeto</label>
                                 </div>
                                 <br/>
 
-                                <select className="form-select" id="category" onChange={this.handleChange} >
-                                    <option selected >{this.state.category}</option>
+                                <select className="form-select" id="category" onChange={this.handleChange} value={this.state.category} >
                                     {this.renderCategories()}
                                 </select>
                                 <br/>
