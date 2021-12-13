a6312febe9a2d5ea1c0ddb2cb54ad4441c9b2205
lisa31419 <isa31419@gmail.com>
2021-12-13 16:50:10 +0100

    First page changes to home page if not logged out

--- frontend/src/components/Start/FirstPage.js
+++ frontend/src/components/Start/FirstPage.js
@@ -3,6 +3,20 @@ import FirstPageContent from './FirstPageContent';
 
 
 class FirstPage extends Component {
+    constructor(props) {
+        super(props);
+        this.checkIfUser = this.checkIfUser.bind(this);
+    }
+
+    componentDidMount() {
+        this.checkIfUser();
+    }
+
+    checkIfUser () {
+        if (window.localStorage.getItem('user_id') !== null) {
+            window.location.assign("/homePage")
+        }
+    }
     render(){
         return(
             <section>
