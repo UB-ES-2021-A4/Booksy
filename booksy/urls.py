728f0a3d33d9e41ab5ad0bcd7bcbc9aaa52f7440
Eric <eriduma@gmail.com>
2021-12-14 14:59:37 +0100

    added default.png

--- booksy/urls.py
+++ booksy/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('login/', accounts.views.index, name='index'),
     path('homePage/', accounts.views.index, name='index'),
     path('additem/', accounts.views.index, name='index'),
-    path('profile/(?P<userid>\d+)/$', accounts.views.index, name='index'),
+    path('profile/', accounts.views.index, name='index'),
 
     path('admin/', admin.site.urls),
     path('api/account/', include('accounts.urls')),
