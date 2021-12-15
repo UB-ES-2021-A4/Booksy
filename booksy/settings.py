c4f229bc5292f779cdf461221a3ebbe79a0b22da
Eric <eriduma@gmail.com>
2021-12-15 23:59:22 +0100

    changed db

--- booksy/settings.py
+++ booksy/settings.py
@@ -96,8 +96,7 @@ WSGI_APPLICATION = 'booksy.wsgi.application'
         'PASSWORD': 'z8feEEG@rnZzEYi',
         'HOST': 'booksy.mysql.pythonanywhere-services.com',
         'PORT': '3306',
-    }
-
+'''
     Pythonanywhere
 
 
@@ -121,13 +120,6 @@ if sys.argv[1] == 'test':
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    '''
-    DATABASES = {
-        'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'd7cm11lo9q6vmh',
             'USER': 'bothhazwmejtql',
@@ -135,7 +127,15 @@ else:
             'HOST': 'ec2-54-195-76-73.eu-west-1.compute.amazonaws.com',
             'PORT': '5432',
         }
-    }'''
+    }
+'''
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+'''
 
 
 # Media folder
